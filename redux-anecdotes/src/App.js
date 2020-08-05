fc70b74be6968d086a928f5209392f87dde56bf9
Jessica Isoton <jessicasampaio195@gmail.com>
2020-08-05 11:55:12 +0200

    Fixed notification box

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -4,14 +4,13 @@ import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
 import Filter from "./components/Filter";
 const App = () => {
-
-return (
+  return (
     <div>
       <h2>Anecdotes</h2>
       <Notification />
       <Filter />
+      <AnecdoteList />
       <AnecdoteForm />
-      <AnecdoteList />  
     </div>
   );
 };
--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -10,7 +10,7 @@ const Notification = () => {
     borderWidth: 1,
   };
 
-  return <div style={style}>{notification}</div>;
+  return <div hidden= {notification == ""} style={style}>{notification}</div>;
 };
 
 export default Notification;
